83ddb44499f97fb363c47faab4150a40f365e3fc
Dimitry Rudenko <3952800@gmail.com>
2020-06-21 16:25:30 +0200

    convertToRoman refactored

--- src/main/java/RomanNumbers.java
+++ src/main/java/RomanNumbers.java
@@ -1,45 +1,24 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class RomanNumbers {
 
+    private Map<Integer, String> correlation = new LinkedHashMap<Integer, String>(){{
+        put(10, "X");
+        put(9, "IX");
+        put(5, "V");
+        put(4, "IV");
+        put(1, "I");
+    }};
+
     public String convertToRoman(int number) {
-
-        int modulo = number;
-
-
         String result = "";
-        Map<Integer, String> correlation = new HashMap<Integer, String>(){{
-            put(10, "X");
-            put(9, "IX");
-            put(5, "V");
-            put(4, "IV");
-            put(1, "I");
-        }};
-
-
-        int value = 10;
-
-        if (modulo >= value) {
-            result += correlation.get(value);
-            modulo -= value;
+        for (Map.Entry<Integer, String> entry : correlation.entrySet()) {
+            if (number >= entry.getKey()) {
+                result += correlation.get(entry.getKey()).repeat((int)(number / entry.getKey()));
+                number -= entry.getKey();
+            }
         }
-
-
-        value = 9;
-        if (modulo >= value) {
-            result += correlation.get(value);
-            modulo -= value;
-        }
-
-        value = 5;
-        if (modulo >= 5) {
-            result += correlation.get(value);
-            modulo -= value;
-        }
-
-        result += modulo == 4 ? "IV" : "I".repeat(modulo);
-
         return result;
     }
 
